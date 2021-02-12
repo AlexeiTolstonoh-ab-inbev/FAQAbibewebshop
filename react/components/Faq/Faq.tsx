41c1aed2276542d6fd817252bc36b6ae0b06158b
AlexeiTolstonog-ab-inbev <Alexei.Tolstonoh@ab-inbev.com>
2021-02-12 12:16:13 +0200

    product chooser

--- react/components/Faq/Faq.tsx
+++ react/components/Faq/Faq.tsx
@@ -1,88 +1,85 @@
-import React from 'react'
-import styles from './Faq.css'
-interface FaqProps {}
+import React, {useState} from 'react'
 
-const Faq: StorefrontFunctionComponent<FaqProps> = () => {
+
+
+const Faq = () => {
+	// const [brandOpt, setBrandOpt] = useState('/jupiler')
+	const [packType, setPackType] = useState('/pack-type/cans')
+	const [departamentName, setDepartamentName] = useState('/beer')
+	const [type, setType] = useState('/pils')
+
+
+	const departaments = [
+		{ value: '/beer', label: 'Beer' },
+		{ value: '/other', label: 'Other drinks' },
+		{ value: '/jupiler', label: 'Deals' },
+		{ value: '/jupiler', label: 'Merch' },
+	]
+	// const brands = [
+	// 	{ value: '/jupiler', label: 'Jupiler' },
+	// 	{ value: '/stella-artois', label: 'Stella Artois'  },
+	// 	{ value: '/leffe', label: 'Leffe' },
+	// 	{ value: '/vtrimpel', label: 'Tripel Karmeliet' },
+	// 	{ value: '/corona', label: 'Corona'},
+	// 	{ value: '/victoria', label: 'Victoria' },
+	// 	{ value: '/mike\'s', label: 'Mike'},
+	// 	{ value: '/atom', label: 'ATOM Brands' }
+	// ]
+	const types = [
+		{ value: '/pils', label: 'Pils',departament: '/beer'},
+		{ value: '/abbey', label: 'Abbey', departament: '/beer'},
+		{ value: '/Alcoholfree', label: 'Alcoholfree', departament: '/beer'},
+		{ value: '/Strong-Blonde', label: 'Strong Blonde', departament: '/beer' },
+		{ value: '/craft', label: 'Craft', departament: '/beer' },
+		{ value: '/water', label: 'Water', departament: '/other'},
+		{ value: '/water', label: 'Soft Drinks', departament: '/other'},
+		{ value: '/water', label: 'Juices', departament: '/other'},
+		{ value: '/water', label: 'Wine And More', departament: '/other'},
+		{ value: '/water', label: 'Spirits', departament: '/other'},
+		{ value: '/water', label: 'Seltzers', departament: '/other'}
+	]
+	const packTypes: any[] = [
+		{ value: '/pack-type/cans', label: 'Cans',departament: '/beer'},
+		{ value: '/pack-type/bottles', label: 'Bottles', departament: '/beer'},
+		{ value: '/pack-type/perfect-draft', label: 'Perfect Draft', departament: '/beer'},
+		{ value: '/gift', label: 'Gift Packs', departament: '/beer' }
+	]
+	const handleChangeType = (event: any) => {
+		setType(event.target.value);
+	};
+	const handleChangePackType = (event: any) => {
+
+		setPackType(event.target.value);
+	};
+	const handleChangeDepartament = (event: any) => {
+		setDepartamentName(event.target.value)
+	}
+	const typeOfPack = packTypes.filter((el:any)=> el.departament === departamentName)
+	const typeOfSelect = types.filter((el:any) => el.departament === departamentName)
+debugger
+	const linkString = `${departamentName}${packType}${type}`
+
+
+
 	return (
 		<div>
-			<div className={styles.faqWrapper}>
 
-				<div className={styles.faqContainer}>
-					<h3 className={styles.faqContainerHeader}>Order & Receive</h3>
-					<input className={styles.hide} id="hd-1" type="checkbox"></input>
-					<label className={styles.categoryHeader} htmlFor="hd-1">When do I receive my order?</label>
-					<div>
-						HTML — стандартный язык разметки документов во Всемирной паутине. Большинство веб-страниц
-						содержат описание разметки на языке HTML (или XHTML). Язык HTML интерпретируется браузерами и
-						отображается в виде документа в удобной для человека форме..
-					</div>
-					<br/>
-					<br/>
-					<input className={styles.hide} id="hd-2" type="checkbox"></input>
-					<label className={styles.categoryHeader} htmlFor="hd-2">What should I do when the product is out of stock?</label>
-					<div>
-						CSS - CSS Cascading Style Sheets — каскадные таблицы стилей — формальный язык описания
-						внешнего вида документа, написанного с использованием языка разметки. Преимущественно
-						используется как средство описания, оформления внешнего вида веб-страниц, написанных с
-						помощью языков разметки HTML и XHTML, но может также применяться к любым XML-документам,
-						например, к SVG или XUL.
-					</div>
-				</div>
+			<select onChange={handleChangeDepartament}>
+				{departaments.map((el: any) => <option value={el.value} >{el.label}</option>)}
+			</select>
+			{typeOfPack.length>0 ? <select onChange={handleChangePackType}>
+				{typeOfPack.map((el: any) => <option value={el.value} >{el.label}</option>)}
+			</select> : null}
+			<select onChange={handleChangeType}>
+				{typeOfSelect.map((el: any) => <option value={el.value} >{el.label}</option>)}
+			</select>
 
-				<div className={styles.faqContainer}>
-					<h3 className={styles.faqContainerHeader}>Return</h3>
-
-					<input className={styles.hide} id="hd-3" type="checkbox"></input>
-					<label className={styles.categoryHeader} htmlFor="hd-3">When do I receive my order?</label>
-					<div>
-						HTML — стандартный язык разметки документов во Всемирной паутине. Большинство веб-страниц
-						содержат описание разметки на языке HTML (или XHTML). Язык HTML интерпретируется браузерами и
-						отображается в виде документа в удобной для человека форме..
-					</div>
-					<br/>
-					<br/>
-					<input className={styles.hide} id="hd-4" type="checkbox"></input>
-					<label className={styles.categoryHeader} htmlFor="hd-4">What should I do when the product is out of stock?</label>
-					<div>
-						CSS - CSS Cascading Style Sheets — каскадные таблицы стилей — формальный язык описания
-						внешнего вида документа, написанного с использованием языка разметки. Преимущественно
-						используется как средство описания, оформления внешнего вида веб-страниц, написанных с
-						помощью языков разметки HTML и XHTML, но может также применяться к любым XML-документам,
-						например, к SVG или XUL.
-					</div>
-				</div>
-
-				<div className={styles.faqContainer}>
-					<h3 className={styles.faqContainerHeader}>Payment Methods</h3>
-
-					<input className={styles.hide} id="hd-5" type="checkbox"></input>
-					<label className={styles.categoryHeader} htmlFor="hd-5">When do I receive my order?</label>
-					<div>
-						HTML — стандартный язык разметки документов во Всемирной паутине. Большинство веб-страниц
-						содержат описание разметки на языке HTML (или XHTML). Язык HTML интерпретируется браузерами и
-						отображается в виде документа в удобной для человека форме..
-					</div>
-					<br/>
-					<br/>
-					<input className={styles.hide} id="hd-6" type="checkbox"></input>
-					<label className={styles.categoryHeader} htmlFor="hd-6">What should I do when the product is out of stock?</label>
-					<div>
-						CSS - CSS Cascading Style Sheets — каскадные таблицы стилей — формальный язык описания
-						внешнего вида документа, написанного с использованием языка разметки. Преимущественно
-						используется как средство описания, оформления внешнего вида веб-страниц, написанных с
-						помощью языков разметки HTML и XHTML, но может также применяться к любым XML-документам,
-						например, к SVG или XUL.
-					</div>
-				</div>
-
-			</div>
+			<a href={linkString}>Click me</a>
 		</div>
 	)
 }
-Faq.schema = {
-	title: 'editor.faq.title',
-	description: 'editor.faq.description',
-	type: 'object',
-	properties: {},
-}
+
 export default Faq
+//`/beer/bottles/jupiler/pack-type?fuzzy=0&initialMap=c,c,c&initialQuery=beer/pack-type/bottles&map=category-1,category-3,brand,category-2&operator=and`
+//`/beer/bottles/pack-type/stella-artois?fuzzy=0&initialMap=c,c,c&initialQuery=beer/pack-type/bottles&map=category-1,category-3,category-2,brand&operator=and`
+//`/beer/bottles/pack-type/victoria?fuzzy=0&initialMap=c,c,c&initialQuery=beer/pack-type/bottles&map=category-1,category-3,category-2,brand&operator=and`
